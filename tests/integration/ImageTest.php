8f4f57315e2a93d86da38fe5b757227351798ee4
Moshe Weitzman <weitzman@tejasa.com>
2024-10-30 23:46:37 -0400

    Delay getting $application because that can make entitytypeManager stale

--- tests/integration/ImageTest.php
+++ tests/integration/ImageTest.php
@@ -7,8 +7,6 @@ namespace Unish;
 use Drush\Commands\core\ImageCommands;
 use Drush\Commands\core\ImageFlushCommand;
 use Drush\Commands\pm\PmCommands;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Tester\ApplicationTester;
 
 /**
@@ -20,15 +18,17 @@ class ImageTest extends UnishApplicationTesterTestCase
 {
     public function testImage()
     {
-        // We aren't testing pm:install so don't use ApplicationTester yet.
-        // This is the recommended approach from https://symfony.com/doc/current/console/calling_commands.html
-        $input = new ArrayInput([
-            'command' => PmCommands::INSTALL,
-            'modules' => ['image'],
-        ]);
-        $output = new NullOutput();
+        $this->drush(PmCommands::INSTALL, ['image']);
         $application = $this->getApplication();
-        $returnCode = $application->doRun($input, $output);
+        // We aren't testing pm:install so don't use ApplicationTester.
+        // This is the recommended approach from https://symfony.com/doc/current/console/calling_commands.html
+        // We can't actually do this until pm:install moves from an AnnotatedCommand.
+//        $input = new ArrayInput([
+//            'command' => PmCommands::INSTALL,
+//            'modules' => ['image'],
+//        ]);
+//        $output = new NullOutput();
+//        $returnCode = $application->doRun($input, $output);
 
         $logo = 'core/misc/menu-expanded.png';
         $styles_dir = $this->webroot() . '/sites/default/files/styles/';
@@ -41,6 +41,7 @@ class ImageTest extends UnishApplicationTesterTestCase
 
         // Remove stray files left over from previous runs
         @unlink($thumbnail);
+        $this->assertFileDoesNotExist($thumbnail);
 
         // Test that "drush image-derive" works.
         $style_name = 'thumbnail';
@@ -49,7 +50,8 @@ class ImageTest extends UnishApplicationTesterTestCase
 
         // Test that "drush image-flush thumbnail" deletes derivatives created by the thumbnail image style.
         $applicationTester = new ApplicationTester($application);
-        $applicationTester->run([ImageFlushCommand::NAME, 'style_names' => $style_name]);
+        $applicationTester->run([ImageFlushCommand::NAME, 'style_names' => $style_name, '--no-interaction' => true]);
+        $output = $applicationTester->getDisplay();
         $this->assertFileDoesNotExist($thumbnail);
         // @todo note stdin testing documented at https://github.com/symfony/symfony/issues/37835
 
